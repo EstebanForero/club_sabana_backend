69c023e0dfb5a3108f83df4114741cfbdf4e1f7e
RustMaster-arch <rustmasterfullstack@gmail.com>
2025-02-10 16:47:45 -0500

    errors bye

--- turso_db/src/training_repo/mod.rs
+++ turso_db/src/training_repo/mod.rs
@@ -1,8 +1,10 @@
 use async_trait::async_trait;
-use chrono::NaiveDateTime;
 use entities::training::Training;
 use libsql::{de, params};
-use use_cases::training_service::{err::Result, repository_trait::TrainingRepository};
+use use_cases::training_service::{
+    err::{Error, Result},
+    repository_trait::TrainingRepository,
+};
 use uuid::Uuid;
 
 use crate::TursoDb;
@@ -10,7 +12,10 @@ use crate::TursoDb;
 #[async_trait]
 impl TrainingRepository for TursoDb {
     async fn create_training(&self, training: &Training) -> Result<()> {
-        let conn = self.get_connection().await?;
+        let conn = self
+            .get_connection()
+            .await
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         conn.execute(
             "INSERT INTO 
@@ -32,34 +37,38 @@ training (id_training, name, start_datetime, end_datetime, minimum_payment, dele
             ],
         )
         .await
-        .map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?;
+        .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         Ok(())
     }
 
     async fn get_training_by_id(&self, id: Uuid) -> Result<Option<Training>> {
-        let conn = self.get_connection().await?;
+        let conn = self
+            .get_connection()
+            .await
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         let mut rows = conn.query("SELECT id_training, name, start_datetime, end_datetime, minimum_payment, deleted, id_category
-FROM training WHERE id_training = 1?", params![id.to_string()]).await.map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?;
+FROM training WHERE id_training = 1?", params![id.to_string()]).await.map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         if let Some(res_rows) = rows
             .next()
             .await
-            .map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?
         {
-            return Ok(Some(de::from_row::<Training>(&res_rows).map_err::<Box<
-                dyn std::error::Error,
-            >, _>(
-                |err| Box::new(err),
-            )?));
+            return Ok(Some(
+                de::from_row::<Training>(&res_rows)
+                    .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?,
+            ));
         }
-
         Ok(None)
     }
 
     async fn update_training(&self, training: &Training) -> Result<()> {
-        let conn = self.get_connection().await?;
+        let conn = self
+            .get_connection()
+            .await
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         conn.execute(
             "UPDATE training SET name = 2?, start_datetime = 3?, end_datetime = 4?,
@@ -81,40 +90,46 @@ minimum_payment = 5?, deleted = 6?, id_category = 7? WHERE id_training = 1?",
             ],
         )
         .await
-        .map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?;
+        .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         Ok(())
     }
 
     async fn delete_training(&self, id: Uuid) -> Result<()> {
-        let conn = self.get_connection().await?;
+        let conn = self
+            .get_connection()
+            .await
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         conn.execute(
             "UPDATE training SET deleted = 1 WHERE id_training = 1?",
             params![id.to_string()],
         )
         .await
-        .map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?;
+        .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         Ok(())
     }
 
     async fn list_trainings(&self) -> Result<Vec<Training>> {
-        let conn = self.get_connection().await?;
+        let conn = self
+            .get_connection()
+            .await
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         let mut rows = conn.query("SELECT id_training, name, start_datetime, end_datetime, minimum_payment, deleted, id_category FROM
-training", params![]).await.map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?;
+training", params![]).await.map_err(|err| Error::UnknownDatabaseError(err.to_string()))?;
 
         let mut res: Vec<Training> = Vec::new();
 
         while let Some(res_row) = rows
             .next()
             .await
-            .map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?
+            .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?
         {
             res.push(
                 de::from_row::<Training>(&res_row)
-                    .map_err::<Box<dyn std::error::Error>, _>(|err| Box::new(err))?,
+                    .map_err(|err| Error::UnknownDatabaseError(err.to_string()))?,
             );
         }
 
--- use_cases/src/training_service/err.rs
+++ use_cases/src/training_service/err.rs
@@ -5,5 +5,5 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("Unknow error in the database")]
-    UnknownDatabaseError(#[from] Box<dyn std::error::Error>),
+    UnknownDatabaseError(String),
 }
